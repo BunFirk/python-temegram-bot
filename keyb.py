7964224e7b1183a20ce1dff8317fe269edda3075
Guffi-2 <76037676+Guffi-2@users.noreply.github.com>
2021-11-05 21:06:36 +0700

    v1.4

--- keyb.py
+++ keyb.py
@@ -9,8 +9,8 @@ Mnum_1 = ReplyKeyboardMarkup().add(btnnum_1).add(btnnum_7)
 btnnum2 = KeyboardButton("/Курсы_валют_📈")
 btnnum3 = KeyboardButton("/📰_Новости")
 btnnum4 = KeyboardButton("/⚙_Настройки")
-btnnum21 = KeyboardButton("Текст")
-Mnum_2 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum2).add(btnnum3).add(btnnum4)
+btnnum21 = KeyboardButton("/📈_Акции")
+Mnum_2 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum2).add(btnnum3).add(btnnum21).add(btnnum4)
 
 btnnum6 = KeyboardButton("/📈_Крипто")
 btnnum14 = KeyboardButton("/💵_Валюты")
@@ -24,13 +24,34 @@ MNews = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum8).add(btnnum9).add(
 btnnum11 = KeyboardButton("/Ethereum")
 btnnum12 = KeyboardButton("/Bitcoin")
 btnnum13 = KeyboardButton("/Binance_Coin")
-btnnum17 = KeyboardButton("/hash_token")
 MKnum = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum11).add(btnnum12).add(btnnum13).add().add(Back)
 
-btnnum19 = KeyboardButton("/Узнать цену")
-btnnum18 = KeyboardButton("/Вывести график")
-MBitcoin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum18).add(btnnum19).add(Back)
+btnnum23 = KeyboardButton("/Узнать_цену_Bitcoin")
+btnnum18 = KeyboardButton("/Вывести_график_Bitcoin")
+MBitcoin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum18).add(btnnum23).add(Back)
 
 btnnum5 = KeyboardButton("/Выход_🚪")
 btnnum20 = KeyboardButton("/Частота_отправки_цены")
 Mse = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum20).add(btnnum5).add(Back)
+
+MNBitcoin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum23).add(Back)
+
+btnnum22 = KeyboardButton("/Bitcoin_News")
+btnnum26 = KeyboardButton("/Ethereum_News")
+btnnum27 = KeyboardButton("/Binance_Coin_News")
+MnBitcoin_2 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum22).add(btnnum26).add(btnnum27).add(Back)
+
+
+MnBitcoin_3 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum23).add(Back)
+
+btnnum24 = KeyboardButton("/Узнать_цену_Ethereum")
+btnnum25 = KeyboardButton("/Вывести_график_Ethereum")
+MEthereum = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum24).add(btnnum25).add(Back)
+
+MNEthereum = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum24).add(Back)
+
+btnnum28 = KeyboardButton("/Узнать_цену_Binance_Coin")
+MNBinance_Coin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum28).add(Back)
+
+btnnum29 = KeyboardButton("/3M_Company")
+MNBinance_Coin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum29).add(Back)
