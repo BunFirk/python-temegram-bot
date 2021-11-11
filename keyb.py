eadc4ea9d6581858a52b846b4fb09524554bc69f
Guffi-2 <76037676+Guffi-2@users.noreply.github.com>
2021-11-12 06:19:37 +0700

    1.5

--- keyb.py
+++ keyb.py
@@ -52,3 +52,6 @@ MNEthereum = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum24).add(Back)
 
 btnnum28 = KeyboardButton("/Узнать_цену_Binance_Coin")
 MNBinance_Coin = ReplyKeyboardMarkup(resize_keyboard=True).add(btnnum28).add(Back)
+
+
+Ak = ReplyKeyboardMarkup(resize_keyboard=True).add(Back)
